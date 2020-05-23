59607838a10b862f80af828da8836c4d8b023105
1� DAM 4v - Washington Ariel Carrera <wariel_carrera@cuatrovientos.es>
2020-05-23 13:53:33 +0200

    Slider corregido

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,4 +12,32 @@ export class HomePage implements OnInit {
   ngOnInit() {
   }
 
+  // Array de objetos del slider del inicio
+  slides = [
+    {
+      titulo: 'Folding at home <br> Un proyecto de todos',
+      desc: 'Resumen sobre lo que es el proyecto y lo que pretende <br> llegar a conseguir',
+      boton: 'Saber más'
+    },
+    {
+      titulo: 'Comienza a <br> colaborar',
+      desc: 'Información relevante que incite a los lectores a <br> participar en el proyecto',
+      boton: 'Colabora'
+    },
+    {
+      titulo: 'COVID-19',
+      desc: 'Información resumida sobre el covid-19 para aquellos <br> que estén interesados en saber más acerca de ello',
+      boton: 'Saber más'
+    }
+  ];
+
+  // Slider automático
+
+  slideOptionsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: false,
+    speed: 400
+  }
+
 }
