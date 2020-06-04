6cfb27278bf6b065d40d71a313fbee1725e799c7
jsanchez712 <javi.escu97@gmail.com>
2020-06-04 13:25:23 +0200

    Grafico de barras

--- src/app/estadisticas/estadisticas.page.ts
+++ src/app/estadisticas/estadisticas.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-estadisticas',
   templateUrl: './estadisticas.page.html',
   styleUrls: ['./estadisticas.page.scss'],
 })
-export class EstadisticasPage implements OnInit {
-
+export class EstadisticasPage {
+    @ViewChild('barChart') barChart;
+    bars: any;
+    colorArray:any;
   constructor() { }
-
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.createBarChart();
   }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO','SEPTIEMBRE','OCTUBRE','NOVIEMBRE','DICIEMBRE'],
+        datasets: [{
+          label: 'Contributions',
+          data: [2, 3, 5, 6, 6, 7],
+          backgroundColor: 'rgb(255,165,0)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(255,165,0)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+ 
 
 }
