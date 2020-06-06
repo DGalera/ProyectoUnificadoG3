a9e3f2d4591814d672160157b478a13f40a00142
1� DAM 4v - Washington Ariel Carrera <wariel_carrera@cuatrovientos.es>
2020-06-06 13:50:53 +0200

    grafica de estadisticas

--- src/app/estadisticas/estadisticas.page.ts
+++ src/app/estadisticas/estadisticas.page.ts
@@ -16,85 +16,86 @@ export class EstadisticasPage implements OnInit {
   bars: any;
   colorArray: any;
 
+  function1 = false;
+  function2 = false;
+
   public donors: IDonor[];
   public teams: ITeam[];
 
-  private topDonors:  Array<IDonor>;
-  private topDonorsName: string[];
-  private topDonorsCredit: number[];
+  private topDonors:  Array<IDonor> = [];
+  private topDonorsName: string[] = [];
+  private topDonorsCredit: number[] = [];
   dataLoaded = false;
   constructor(private apiService: ApiService, private route: Router) { }
 
   ngOnInit() {
-    
-  this.retrieveValues();
+    this.retrieveValues();
   }
 
+
   retrieveValues(){
-    
-   
      this.apiService.get_Donors().subscribe(
       (data: any) => {
-        this.donors = data.results,
+        this.donors = data.results,    
         this.dataLoaded=true;
       }
-
     );
     return this.donors;
   }
 
   getTopDonorsName(){
-    
     this.topDonors = this.donors.slice(0,10);
-
     for (let i = 0; i < this.topDonors.length; i++) {
-      this.topDonorsName.push(this.topDonors[i].name)
-      
+      this.topDonorsName.push(this.topDonors[i].name);
+      console.log(this.topDonors[i].name); 
     }
     console.log(this.topDonorsName);
   }
+
+
   getTopDonorsCredit(){
- 
   this.topDonors = this.donors.slice(0,10);
     for (let i = 0; i < this.topDonors.length; i++) {
-      this.topDonorsCredit.push(this.topDonors[i].credit)
-      
+      this.topDonorsCredit.push(this.topDonors[i].credit) 
     }
     console.log(this.topDonorsCredit);
   }
-  ionViewDidEnter() {
-    
- 
-   
-    this.createBarChart();
 
+
+  ionViewDidEnter() {
+    setTimeout(()=>{
+      this.createBarChart();
+    }, 1000);
+    
   }
 
+
+  
   createBarChart() {
     this.getTopDonorsName();
     this.getTopDonorsCredit();
-    this.bars = new Chart(this.barChart.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: this.topDonorsName,
-        datasets: [{
-          label: 'Top Donator Credits',
-          data: this.topDonorsCredit,
-          backgroundColor: 'rgb(255,165,0)', // array should have same number of elements as number of dataset
-          borderColor: 'rgb(255,165,0)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+      this.bars = new Chart(this.barChart.nativeElement, {
+        type: 'bar',
+        data: {
+          labels: this.topDonorsName,
+          datasets: [{
+            label: 'Top Donator Credits',
+            data: this.topDonorsCredit,
+            backgroundColor: 'rgb(255,165,0)', // array should have same number of elements as number of dataset
+            borderColor: 'rgb(255,165,0)',// array should have same number of elements as number of dataset
+            borderWidth: 1
           }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
         }
-      }
-    });
-  }
+      });
+    }   
 
 }
