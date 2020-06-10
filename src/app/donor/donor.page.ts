5113e79bb4688ea5d308c0183d746cf4e1608c29
zazpe9 <davidzazpegutierrez@gmail.com>
2020-06-10 04:18:04 +0200

    .

--- src/app/donor/donor.page.ts
+++ src/app/donor/donor.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IDonor } from '../share/interfaces';
+import { IDonor, ITeam } from '../share/interfaces';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../api/api.service';
 import { ToastController } from '@ionic/angular';
@@ -10,35 +10,101 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./donor.page.scss'],
 })
 export class DonorPage implements OnInit {
-
   public name: string;
   public donor: IDonor;
   donorExists = false;
   error = false;
+  public teams: ITeam[];
+  numTimesLeft = 0;
+  firstTime: boolean;
+  rangeInitial = 0;
+  rangeFinal = 6;
+  rest = 0;
+  lastRound = false;
+  smallerThanSix: boolean;
+
+
 
   constructor(
     private activatedrouter: ActivatedRoute,
     private router: Router,
     private apiService: ApiService,
     public toastController: ToastController
-    
-  ) { }
+
+  ) { this.teams = [], this.firstTime = true, this.smallerThanSix = false }
+
+
 
   ngOnInit() {
     this.name = this.activatedrouter.snapshot.params.name;
 
     this.apiService.get_Donor(this.name).subscribe(
-      (data: any) =>{
+      (data: any) => {
         this.donor = data
         this.donorExists = true
         this.error = false
+        this.addMoreTeams()
       },
-      (err) =>{
+      (err) => {
         console.log("Error: Donor not found")
         this.donorExists = false
         this.error = true;
-      } 
+      }
     )
+
   }
 
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.addMoreTeams();
+      infiniteScroll.target.complete();
+    }, 500);
+
+  }
+
+  addMoreTeams() {
+    if (this.firstTime == true) {
+      if(this.donor.teams.length<6){
+        this.smallerThanSix = true
+        for (let i = 0; i < this.donor.teams.length; i++) {
+          this.teams.push(this.donor.teams[i]);
+        }
+      }else{
+        this.numTimesLeft = Math.trunc(this.donor.teams.length / 6);
+        this.rest = this.donor.teams.length % 6;
+        
+      }
+      
+    }
+
+    this.firstTime = false
+    
+    if(!this.smallerThanSix){
+      for (let i = this.rangeInitial; i < this.rangeFinal; i++) {
+        this.teams.push(this.donor.teams[i]);
+      }
+  
+      if(this.lastRound==true){
+        this.rest=0
+      }
+  
+      this.numTimesLeft -= 1
+      console.log(this.numTimesLeft)
+  
+      if (this.numTimesLeft <= 0) {
+        this.rangeFinal += this.rest
+        this.lastRound = true;
+        console.log("Last round")
+      } else {
+        this.rangeFinal += 6
+      }
+      this.rangeInitial += 6
+    }
+    
+
+
+  }
 }
--- src/app/team/team.page.ts
+++ src/app/team/team.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ITeam } from '../share/interfaces';
+import { ITeam, IDonor } from '../share/interfaces';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../api/api.service';
 import { ToastController } from '@ionic/angular';
@@ -15,28 +15,94 @@ export class TeamPage implements OnInit {
   public team: ITeam;
   teamExists = false;
   error = false;
+  public donors: IDonor[];
+  numTimesLeft;
+  firstTime: boolean;
+  rangeInitial = 0;
+  rangeFinal = 6;
+  rest;
+  lastRound = false;
+  smallerThanSix: boolean;
+
 
   constructor(
     private activatedrouter: ActivatedRoute,
     private router: Router,
     private apiService: ApiService,
     public toastController: ToastController
-  ) { }
+  ) { this.donors = [], this.firstTime = true, this.smallerThanSix = false }
+
 
   ngOnInit() {
     this.id = this.activatedrouter.snapshot.params.id;
     this.apiService.get_Team(this.id).subscribe(
-      (data: any) =>{
+      (data: any) => {
         this.team = data
         this.teamExists = true
         this.error = false
+        this.addMoreDonors()
       },
-      (err) =>{
+      (err) => {
         console.log("Error: Team not found")
         this.teamExists = false
         this.error = true;
-      } 
+      }
     )
   }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.addMoreDonors();
+      infiniteScroll.target.complete();
+    }, 500);
+
+  }
+
+  addMoreDonors() {
+
+    if (this.firstTime == true) {
+      if (this.team.donors.length < 6) {
+        this.smallerThanSix = true
+        for (let i = 0; i < this.team.donors.length; i++) {
+          this.donors.push(this.team.donors[i]);
+        }
+      } else {
+        this.numTimesLeft = Math.trunc(this.team.donors.length / 6);
+        this.rest = this.team.donors.length % 6;
+        console.log(this.rest)
+        
+      }
+
+    }
+
+    this.firstTime = false
+
+    if (!this.smallerThanSix) {
+      for (let i = this.rangeInitial; i < this.rangeFinal; i++) {
+        this.donors.push(this.team.donors[i]);
+      }
+
+      if (this.lastRound == true) {
+        this.rest = 0
+      }
+
+      this.numTimesLeft -= 1
+      console.log(this.numTimesLeft)
+
+      if (this.numTimesLeft <= 0) {
+        this.rangeFinal += this.rest
+        this.lastRound = true;
+        console.log("Last round")
+      } else {
+        this.rangeFinal += 6
+      }
+      this.rangeInitial += 6
+
+    }
+  }
+
 
 }
