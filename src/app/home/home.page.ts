d5bc11efce6d261bddd35b13426a6634ca9b9ad6
jsanchez712 <javi.escu97@gmail.com>
2020-06-12 19:35:00 +0200

    fotos slider

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { LoginPage } from '../login/login.page';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-home',
@@ -20,19 +21,22 @@ export class HomePage implements OnInit {
       titulo: 'Folding at home <br> Un proyecto de todos',
       desc: 'Información sobre las contribuciones en <br> Folding at Home',
       boton: 'Estadísticas',
-      navigate: '/estadisticas'
+      navigate: '/estadisticas',
+      foto: './../../assets/img/fotofolding.jpg'
     },
     {
       titulo: 'Comienza a <br> colaborar',
       desc: 'Descubre como participar en el proyecto',
       boton: 'Colabora',
-      navigate: '/colaborar'
+      navigate: '/colaborar',
+      foto:'./../../assets/img/folding-at-home.jpg'
     },
     {
       titulo: 'Visita nuestro FORO',
       desc: 'Y háznos una pregunta',
       boton: 'Respuestas',
-      navigate: '/tablon-respuestas'
+      navigate: '/tablon-respuestas',
+      foto: './../../assets/img/slide.jpg'
     }
   ];
 
